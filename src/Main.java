8672c4345e09dd9f3de9d5016ef1e6d994ace0e2
catalinganea <catalin.ganea@student.unitbv.ro>
2023-05-29 17:41:53 +0300

    creare fisier principal

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+import laborator_2.*;
+
+import java.security.NoSuchAlgorithmException;
+import java.text.ParseException;
+
+public class Main {
+    public static void main(String[] args) throws ParseException, NoSuchAlgorithmException {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        exercitiu_1.msg();
+    }
+}
