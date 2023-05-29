ef69600f3ed7ffa2b24d3a5346d28964b0918ee9
catalinganea <catalin.ganea@student.unitbv.ro>
2023-05-29 17:29:29 +0300

    exercitiile 1,2,3,4,5

---
+++ src/laborator_4/exercitiu_1.java
@@ -0,0 +1,31 @@
+package laborator_4;
+
+import java.util.Scanner;
+
+public class exercitiu_1 {
+    public static void msg() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scanner.nextInt();
+
+        int[] array = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            if (array[i] > largest) {
+                secondLargest = largest;
+                largest = array[i];
+            } else if (array[i] > secondLargest && array[i] != largest) {
+                secondLargest = array[i];
+            }
+        }
+
+        System.out.println("The second largest element in the array is: " + secondLargest);
+    }
+}
