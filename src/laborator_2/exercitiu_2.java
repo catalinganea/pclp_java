5f6f74ceb2230a801ba7a27d3173d7ddda4ddf85
catalinganea <catalin.ganea@student.unitbv.ro>
2023-05-29 17:26:47 +0300

    exercitiile 1,2,3,4,5

---
+++ src/laborator_2/exercitiu_2.java
@@ -0,0 +1,27 @@
+package laborator_2;
+
+import java.util.Scanner;
+
+public class exercitiu_2 {
+    public static void msg() {
+        int number;
+        int sum = 0;
+        int get_num;
+
+        // Create a Scanner object to read input.
+        Scanner console = new Scanner(System.in);
+
+        // Get number from the user.
+        System.out.print("Please enter a number ");
+        number = console.nextInt();
+
+        // Display sum
+        get_num = number;
+        while (get_num > 0) {
+            sum += get_num % 10;
+            get_num /= 10;
+        }
+
+        System.out.println("The sum of all digits in " + number + " is " + sum);
+    }
+}
